9ec09bdf7c310184d269c900f4a343c0837867de
Nipun mahajan <nipunmahajan425201@gmail.com>
2025-06-05 10:08:53 +0530

    added diagonal matrix sum

---
+++ 3_3matrixdiagonalsum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include <limits.h>
+
+using namespace std;
+
+int main(){ 
+    int row=3 ,col =3;
+    int matrix[3][3];
+    cout<<"enter the values of matrix for 3*3 matrix"<<endl;
+    for(int i=0; i<row;i++){
+        for(int j=0; j<col;j++){
+            cin>>matrix[i][j];
+        }
+    }
+    int diagonalsum=0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(j==i){
+                diagonalsum =diagonalsum+matrix[i][j];
+            }
+            
+        }
+    }
+    cout<<"diagonal sum is "<<diagonalsum<<endl;
+    
+
+
+}
